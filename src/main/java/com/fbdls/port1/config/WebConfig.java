f5ff3788e81b6afa8ef91013b91adf0af56a305b
fbdlsgud <dlsgud4865@gmail.com>
2025-05-02 01:23:31 +0900

    배포전되돌림

--- src/main/java/com/fbdls/port1/config/WebConfig.java
+++ src/main/java/com/fbdls/port1/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 요청 경로에 대해
-                .allowedOrigins("http://localhost:3000") // 허용할 프론트 주소
+                .allowedOrigins("http://localhost:3000"
+                        ) // 허용할 프론트 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 방식
                 .allowedHeaders("*") // 어떤 헤더든 허용
                 .allowCredentials(true); // 쿠키 등 인증 포함 허용
