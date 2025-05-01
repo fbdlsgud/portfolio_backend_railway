83374c94c82454593cdd21809762ec0ceef9c682
fbdlsgud <dlsgud4865@gmail.com>
2025-05-02 04:15:01 +0900

    cors 수정

--- src/main/java/com/fbdls/port1/config/WebConfig.java
+++ src/main/java/com/fbdls/port1/config/WebConfig.java
@@ -1,20 +1,29 @@
 package com.fbdls.port1.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 요청 경로에 대해
-                .allowedOrigins("http://localhost:3000","https://portfolio-start-ten.vercel.app"
-                        ) // 허용할 프론트 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 방식
-                .allowedHeaders("*") // 어떤 헤더든 허용
-                .allowCredentials(true); // 쿠키 등 인증 포함 허용
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://portfolio-start-ten.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
+
