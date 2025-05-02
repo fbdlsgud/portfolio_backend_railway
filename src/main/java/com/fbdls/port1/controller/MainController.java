8ec7ff6cfc2496bc4889931cdceeb259752659b4
fbdlsgud <dlsgud4865@gmail.com>
2025-05-02 11:14:12 +0900

    main

--- src/main/java/com/fbdls/port1/controller/MainController.java
+++ src/main/java/com/fbdls/port1/controller/MainController.java
@@ -1,1 +1,14 @@
+package com.fbdls.port1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "✅ I'm alive!";
+    }
+
+}
