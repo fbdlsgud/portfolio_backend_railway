dc9d5512b370657704fc7f47a6c914c1917722f0
fbdlsgud <dlsgud4865@gmail.com>
2025-05-02 11:54:58 +0900

    do

--- src/main/java/com/fbdls/port1/config/WebConfig.java
+++ src/main/java/com/fbdls/port1/config/WebConfig.java
@@ -30,7 +30,8 @@ public class WebConfig {
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(true)  // ✅ 이거 필수
+                        .exposedHeaders("Set-Cookie");  // ✅ 인증 쿠키 주고받기 위함
             }
         };
     }
