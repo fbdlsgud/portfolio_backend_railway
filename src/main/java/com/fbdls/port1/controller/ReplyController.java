597863ffd19dad32cd7af118d816c5db552a4db2
fbdlsgud <dlsgud4865@gmail.com>
2025-05-06 17:28:00 +0900

    replyController 어너테이션을 레포지토리로 잘못씀! 수정완로

--- src/main/java/com/fbdls/port1/controller/ReplyController.java
+++ src/main/java/com/fbdls/port1/controller/ReplyController.java
@@ -4,14 +4,15 @@ import com.fbdls.port1.entity.Reply;
 import com.fbdls.port1.service.ReplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Repository
+@RestController
 public class ReplyController {
 
 
